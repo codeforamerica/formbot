b41d8d0564118159c566259ba0bc2bb2cf4dc53d
Prashant Singh <prashant@codeforamerica.org>
2012-04-02 19:07:51 -0700

    Turn makeform.py and readform.py into executable tools.

--- makeform.py
+++ makeform.py
@@ -1,51 +1,64 @@
+#!/usr/bin/env python
 from PIL import Image
 import formbot.bubble as Bubble
 import formbot.regmark as rm
 import json
+import sys
+import getopt
 
+def usage():
+  print("Usage:")
+  print("makeform.py -i <skeletonfile> -d <formdatafile> -o <outputfile>")
 
-SKELETON = "skeletonform01.tiff"
-FORMDATA = "formdata01.json"
-OUTFILE = "form01.tiff"
+def main(argv):
+  try:
+    opts, args = getopt.getopt(argv, "i:d:o:")
+  except getopt.GetoptError:
+    usage()
+    sys.exit(2)
+  SKELETON = None
+  FORMDATA = None
+  OUTFILE = None
+  for opt, arg in opts:
+    if opt == "-h":
+      usage()
+      sys.exit()
+    elif opt == "-i":
+      SKELETON = arg
+    elif opt == "-d":
+      FORMDATA = arg
+    elif opt == "-o":
+      OUTFILE = arg
+  if not (SKELETON or FORMDATA or OUTFILE):
+    usage()
+    sys.exit(2)
+  # Read the JSON string for the form data from the file
+  formdatafile = file(FORMDATA, "r")
+  # Load the form data
+  formdata = json.load(formdatafile)
+  formdatafile.close()
+  #
+  # TODO: add a barcode to indicate the form id
+  #form_id = formdata["id"]
+  #
+  # Create bubble objects from the form data
+  formsets = Bubble.readform(FORMDATA)
+  #
+  # Load the skeletal form
+  form_img = Image.open(SKELETON)
+  #
+  # Draw the bubbles
+  for bs in formsets.sets:
+    bs.draw(form_img)
+  #
+  # Draw the registration markers according to the data
+  rmarks = rm.readform(FORMDATA)
+  for mark in rmarks:
+    mark.draw(form_img)
+  #
+  # Save the form
+  form_img.save(OUTFILE, "TIFF")
 
-# Read the JSON string for the form data from the file
-formdatafile = file(FORMDATA, "r")
-# Load the form data
-formdata = json.load(formdatafile)
-formdatafile.close()
+if __name__ == "__main__":
+  main(sys.argv[1:])
 
-
-form_id = formdata["id"]
-sets = formdata["sets"]
-regs = formdata["regmarks"]
-
-# Create bubble objects from the form data
-formsets = Bubble.readform(FORMDATA)
-#formsets = []
-#for bsdct in sets:
-#  bs = Bubble.BubbleSet()
-#  for bubdct in bsdct["bubbles"]:
-#    bs.add_bubble(Bubble.Bubble(tuple(bubdct["center"]), bubdct["radius"]))
-#  formsets.append(bs)
-
-# Load the skeletal form
-form_img = Image.open(SKELETON)
-
-# Draw the bubbles
-for bs in formsets.sets:
-  bs.draw(form_img)
-
-# Draw the registration markers according to the data
-rmarks = rm.readform(FORMDATA)
-for mark in rmarks:
-  mark.draw(form_img)
-# XXX
-#r0 = RegMark((50,50,250,250))
-#r1 = RegMark((50,950,250,1150))
-#r2 = RegMark((550,950,750,1150))
-#r0.draw(form_img)
-#r1.draw(form_img)
-#r2.draw(form_img)
-
-# Save the form
-form_img.save(OUTFILE, "TIFF")
--- readform.py
+++ readform.py
@@ -1,32 +1,62 @@
+#!/usr/bin/env python
 from PIL import Image
 import formbot.bubble as Bubble
 import formbot.regmark as rm
 import json
+import sys
+import getopt
 
 
-FORMDATA = "formdata01.json"
+def usage():
+  print("Usage:")
+  print("readform.py -i <inputfile> -d <formdatafile>")
 
-INFILE = "form01_filled02.tiff"
+def main(argv):
+  try:
+    opts, args = getopt.getopt(argv, "i:d:")
+  except getopt.GetoptError:
+    usage()
+    sys.exit(2)
+  INFILE = None
+  FORMDATA = None
+  for opt, arg in opts:
+    if opt == "-h":
+      usage()
+      sys.exit()
+    elif opt == "-i":
+      INFILE = arg
+    elif opt == "-d":
+      FORMDATA = arg
+  if not (INFILE or FORMDATA):
+    usage()
+    sys.exit(2)
+  #
+  # TODO: First we should read the registration mark data, which will apply to the whole set of forms
+  # for a particular survey project. Second, we fix the scanned image. Third, we read the barcode,
+  # so we can look up the bubble set information. Last, we check which bubbles have been filled in.
+  #
+  # Read form data
+  formsets = Bubble.readform(FORMDATA)
+  #
+  # Read registration mark data
+  rmarks = rm.readform(FORMDATA)
+  #
+  # Load the image of the filled-out form
+  form_img = Image.open(INFILE)
+  #
+  # Adjust image to fit the prototype
+  form_img_fixed = rm.fiximage(form_img, rmarks[0], rmarks[1], rmarks[2])
+  #
+  # Check responses to each bubble set
+  responses = []
+  for bs in formsets.sets:
+    responses.append(bs.get_single_answer(form_img_fixed))
+  #
+  print("Form ID: %s" % formsets.form_id)
+  for i in range(len(responses)):
+    print("Response #%s: %s" % (i, responses[i]))
 
-# Read form data
-formsets = Bubble.readform(FORMDATA)
 
-# Read registration mark data
-rmarks = rm.readform(FORMDATA)
-
-# Load the image of the filled-out form
-form_img = Image.open(INFILE)
-
-# Adjust image to fit the prototype
-form_img_fixed = rm.fiximage(form_img, rmarks[0], rmarks[1], rmarks[2])
-
-# Check responses to each bubble set
-responses = []
-for bs in formsets.sets:
-  responses.append(bs.get_single_answer(form_img_fixed))
-
-print("Form ID: %s" % formsets.form_id)
-for i in range(len(responses)):
-  print("Response #%s: %s" % (i, responses[i]))
-
+if __name__ == "__main__":
+  main(sys.argv[1:])
 
