9fa1dc5ddeb20acad517900dcf9c881e3d5fc7e1
Prashant Singh <prashant@codeforamerica.org>
2012-04-12 15:12:01 -0700

    Obey and update the status field

--- process_form.py
+++ process_form.py
@@ -17,11 +17,53 @@ SURVEY_ID = '1'
 # API base URL
 API_BASE = 'http://localhost:3000'
 
-# Image storage base URL
+# Image data base URL
 IMG_BASE = 'http://localhost/~prashant/formbot_images/%s'
+
+class APIError(Exception):
+  def __init__(self, value):
+    self.value = value
+  def __str__(self):
+    return repr(self.value)
+
+
+# Update the status of a scan.
+def update_status(sid, img_id, status):
+  img_info_url = '%s/surveys/%s/scans/%s' % (API_BASE, sid, img_id)
+  try:
+    print 'Updating scan status: %s' % img_info_url
+    data = json.dumps({'scan' : {'status': status}})
+    print 'JSON data:\n%s' % data
+    headers = { 'Content-Type' : 'application/json', 'X-HTTP-Method-Override' : 'PUT' }
+    req = urllib2.Request(img_info_url, headers=headers, data=data)
+    api_resp = urllib2.urlopen(req).read()
+  except urllib2.HTTPError, e:
+    print "HTTP error: %d" %e.code
+    raise APIError(e)
+  except urllib2.URLError, e:
+    print "Network error: %s" % e.reason.args[1]
+    raise APIError(e)
+  print 'Response from server:'
+  print api_resp
+  return api_resp
+
+# Get the actual image
+def get_image(img_url):
+  img_data = None
+  try:
+    print 'Getting image from %s' % img_url
+    img_data = urllib2.urlopen(img_url).read();
+  except urllib2.HTTPError, e:
+    print "HTTP error: %d" %e.code
+    raise APIError(e)
+  except urllib2.URLError, e:
+    print "Network error: %s" % e.reason.args[1]
+    raise APIError(e)
+  return img_data
 
 # Process a form image and record the results to the database
 def record_form(img_id, noact=False):
+  #
   # Get the basic structural information from the survey data
   survey_url = '%s/surveys/%s' % (API_BASE, SURVEY_ID)
   try:
@@ -36,21 +78,46 @@ def record_form(img_id, noact=False):
   print 'Processing registration mark information'
   survey = json.loads(survey_json)['survey']
   rmarks = rm.extract_data(survey['paperinfo'])
-  # Right now we just pass the whole form image to the barcode reader
+  #
+  # Right now we just pass the whole form image to the barcode reader, so we
+  # don't need the barcode bounding box
   #barcode_bbox = survey['paperinfo']['barcode']['bbox'];
   #
-  # Get the image
-  img_url = '%s/surveys/%s/scans/%s' % (API_BASE, SURVEY_ID, img_id)
-  img_data = None
+  # Get the image info
+  img_info_url = '%s/surveys/%s/scans/%s' % (API_BASE, SURVEY_ID, img_id)
+  img_info = None
   try:
-    print 'Getting image from %s' % img_url
-    img_data = urllib2.urlopen(img_url).read()
+    print 'Getting image info from %s' % img_info_url
+    img_info = json.loads(urllib2.urlopen(img_info_url).read())
   except urllib2.HTTPError, e:
     print "HTTP error: %d" %e.code
     return 2
   except urllib2.URLError, e:
     print "Network error: %s" % e.reason.args[1]
     return 2
+  #
+  # Confirm that there is work to be done
+  status = img_info['scan']['status']
+  if status == "complete":
+    print "WARNING: We already processed this image"
+    return
+  if status == "working":
+    print "DOUBLE WARNING: Someone is currently processing this image. Don't run two processing tasks at the same time until the API supports it."
+    return
+  print "Got status %s. It's OK to proceed." % status
+  #
+  # Get the actual image
+  try:
+    img_data = get_image(img_info['scan']['url'])
+  except APIError, e:
+    return 2
+  #
+  # Update status to 'pending'
+  if not noact:
+    try:
+      update_status(SURVEY_ID, img_id, 'working')
+    except APIError, e:
+      return 2
   #
   # Load the image of the filled-out form
   imagestring = StringIO.StringIO(img_data)
@@ -105,7 +172,7 @@ def record_form(img_id, noact=False):
   #
   # Record the data through the API
   if noact:
-    return 0
+    return
   response_url = '%s/surveys/%s/responses' % (API_BASE, SURVEY_ID)
   try:
     print 'Posting responses to %s' % survey_url
@@ -121,6 +188,12 @@ def record_form(img_id, noact=False):
     return 2
   print 'Response from server:'
   print api_resp
+  #
+  # Update status to 'complete'
+  try:
+    update_status(SURVEY_ID, img_id, 'complete')
+  except APIError, e:
+    return 2
 
 def main(argv=None):
   noact = False
