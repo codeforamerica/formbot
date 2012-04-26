1fdbb4182fa6fa446870ba9e28ef836eac1a2626
Prashant Singh <prashant@codeforamerica.org>
2012-04-26 15:13:13 -0700

    Switch from urllib2 to requests.
    The requests module provides a cleaner interface and we already needed
    it elsewhere in the project.
    
    Some minor refactoring to avoid code duplication and to help clarify
    what's going on.

--- process_form.py
+++ process_form.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python
 
-import urllib2
-import urllib
 import StringIO
 import formbot.bubble as Bubble
 import formbot.regmark as rm
@@ -12,6 +10,7 @@ import sys
 import getopt
 import os
 import time
+import requests
 
 # API base URL
 API_BASE = os.getenv('SURVEY_API_BASE', 'http://localhost:3000')
@@ -26,50 +25,35 @@ class APIError(Exception):
   def __str__(self):
     return repr(self.value)
 
+# Get data from the API URL and parse the JSON response
+def api_get(url):
+  try:
+    r = requests.get(url)
+    data = json.loads(r.text)
+  except requests.exceptions.RequestException, e:
+    print 'Error completing the request: %s' % e
+    raise APIError(e)
+  return data
+
 # Get the unprocessed scans
 def get_scans(sid):
-  #
-  # Get the image info
   scans_url = '%s/surveys/%s/scans?status=pending' % (API_BASE, sid)
-  scans = None
-  try:
-    print 'Getting pending scanned image data from %s' % scans_url
-    data = json.loads(urllib2.urlopen(scans_url).read())
-  except urllib2.HTTPError, e:
-    print "HTTP error: %d" %e.code
-    raise APIError(e)
-  except urllib2.URLError, e:
-    print "Network error: %s" % e.reason.args[1]
-    raise APIError(e)
+  print 'Getting pending scans from %s' % scans_url
+  data = api_get(scans_url)
   return data['scans']
 
 # Get all of the surveys
 def get_surveys():
   url = '%s/surveys' % API_BASE
-  ids = None
-  try:
-    print 'Getting surveys from %s' % url
-    data = json.loads(urllib2.urlopen(url).read())
-  except urllib2.HTTPError, e:
-    print "HTTP error: %d" %e.code
-    raise APIError(e)
-  except urllib2.URLError, e:
-    print "Network error: %s" % e.reason.args[1]
-    raise APIError(e)
+  print 'Getting all of the surveys from %s' % url
+  data = api_get(url)
   return data['surveys']
 
 # Get a survey
 def get_survey(sid):
   url = '%s/surveys/%s' % (API_BASE, sid)
-  try:
-    print 'Getting survey %s from url %s' % (sid, url)
-    data = json.loads(urllib2.urlopen(url).read())
-  except urllib2.HTTPError, e:
-    print "HTTP error: %d" %e.code
-    raise APIError(e)
-  except urllib2.URLError, e:
-    print "Network error: %s" % e.reason.args[1]
-    raise APIError(e)
+  print 'Getting survey %s from url %s' % (sid, url)
+  data = api_get(url)
   return data['survey']
 
 # Update the status of a scan.
@@ -80,13 +64,9 @@ def update_status(sid, img_id, status):
     data = json.dumps({'scan' : {'status': status}})
     print 'JSON data:\n%s' % data
     headers = { 'Content-Type' : 'application/json', 'X-HTTP-Method-Override' : 'PUT' }
-    req = urllib2.Request(img_info_url, headers=headers, data=data)
-    api_resp = urllib2.urlopen(req).read()
-  except urllib2.HTTPError, e:
-    print "HTTP error: %d" %e.code
-    raise APIError(e)
-  except urllib2.URLError, e:
-    print "Network error: %s" % e.reason.args[1]
+    r = requests.post(img_info_url, headers=headers, data=data)
+    api_resp = r.text
+  except requests.exceptions.RequestException, e:
     raise APIError(e)
   print 'Response from server:'
   print api_resp
@@ -95,14 +75,12 @@ def update_status(sid, img_id, status):
 # Get the actual image
 def get_image(img_url):
   img_data = None
+  print 'Getting image from %s' % img_url
   try:
-    print 'Getting image from %s' % img_url
-    img_data = urllib2.urlopen(img_url).read()
-  except urllib2.HTTPError, e:
-    print "HTTP error: %d" %e.code
-    raise APIError(e)
-  except urllib2.URLError, e:
-    print "Network error: %s" % e.reason.args[1]
+    r = requests.get(img_url)
+    img_data = r.content
+  except requests.exceptions.RequestException, e:
+    print 'Error completing the request: %s' % e
     raise APIError(e)
   return img_data
 
@@ -112,121 +90,95 @@ def check_work():
   work = False
   try:
     headers = { 'X-Comet-Timeout' : COMET_DELAY }
-    req = urllib2.Request(url, headers=headers)
-    data = json.loads(urllib2.urlopen(req).read())
+    r = requests.get(url, headers=headers)
+    data = json.loads(r.text)
     work = data['haswork']
-  except urllib2.HTTPError, e:
-    print "HTTP error: %d" %e.code
-    raise APIError(e)
-  except urllib2.URLError, e:
-    print "Network error: %s" % e.reason.args[1]
+  except requests.exceptions.RequestException, e:
+    print 'Error completing the request: %s' % e
     raise APIError(e)
   return work
 
 # Process a form image and record the results to the database
-def record_form(survey_id, img_id, noact=False, paperinfo=None):
-  #
-  # Get the basic structural information from the survey data
-  # TODO: refactor so that we don't do this for every iteration
-  survey_url = '%s/surveys/%s' % (API_BASE, survey_id)
-  try:
-    print 'Getting survey information from %s' % survey_url
-    survey_json = urllib2.urlopen(survey_url).read()
-  except urllib2.HTTPError, e:
-    print "HTTP error: %d" %e.code
-    return 2
-  except urllib2.URLError, e:
-    print "Network error: %s" % e.reason.args[1]
-    return 2
-  print 'Processing registration mark information'
-  survey = json.loads(survey_json)['survey']
+def record_form(survey, img_id, noact=False, paperinfo=None):
+  survey_id = survey['id']
   rmarks = rm.extract_data(survey['paperinfo'])
   #
   # Right now we just pass the whole form image to the barcode reader, so we
   # don't need the barcode bounding box
   #barcode_bbox = survey['paperinfo']['barcode']['bbox'];
-  #
+
   # Get the image info
   img_info_url = '%s/surveys/%s/scans/%s' % (API_BASE, survey_id, img_id)
-  img_info = None
+  print 'Getting image info from %s' % img_info_url
   try:
-    print 'Getting image info from %s' % img_info_url
-    img_info = json.loads(urllib2.urlopen(img_info_url).read())
-  except urllib2.HTTPError, e:
-    print "HTTP error: %d" %e.code
+    img_info = api_get(img_info_url)
+  except APIError, e:
     return 2
-  except urllib2.URLError, e:
-    print "Network error: %s" % e.reason.args[1]
-    return 2
-  #
+
   # Confirm that there is work to be done
   status = img_info['scan']['status']
   if status == "complete":
     print "WARNING: We already processed this image"
     return
   if status == "working":
-    print "DOUBLE WARNING: Someone is currently processing this image. Don't run two processing tasks at the same time until the API supports it."
+    print "DOUBLE WARNING: Someone is currently processing this image."
+    print ("Don't run two processing tasks at the same time "
+           "until the API supports it.")
     return
   print "Got status %s. It's OK to proceed." % status
-  #
+
   # Get the actual image
   try:
     img_data = get_image(img_info['scan']['url'])
   except APIError, e:
     return 2
-  #
+
   # Update status to 'working'
   if not noact:
     try:
       update_status(survey_id, img_id, 'working')
     except APIError, e:
       return 2
-  #
+
   # Load the image of the filled-out form
   imagestring = StringIO.StringIO(img_data)
   form_img = Image.open(imagestring)
-  #
+
   # Adjust image to fit the prototype
-  # TODO: After we try fixing the image once, the registration markers are easier to find.
-  # So if we try fixing again, we get closer. This is a little hacky, though. We should make
-  # that part of the registration procedure.
+  # TODO: After we try fixing the image once, the registration markers are
+  # easier to find.  So if we try fixing again, we get closer. This is a little
+  # hacky, though.  We should make that part of the registration procedure.
   print 'Fixing image'
   form_img_fixed = rm.fiximage(form_img, rmarks[0], rmarks[1], rmarks[2])
   form_img_fixed = rm.fiximage(form_img_fixed, rmarks[0], rmarks[1], rmarks[2])
-  #
+
   # Read the barcode
   print 'Reading barcode'
   # Crop out an area around the barcode, to make life easier on the decoder.
-  # XXX
-  print 'paperinfo: %s' % json.dumps(paperinfo)
   bc_bbox = paperinfo['barcode']['bbox']
   dpi = paperinfo['dpi']
   paper_size = (int(8.5*dpi), int(11.0*dpi))
-  crop_bbox = (max(bc_bbox[0] - 50, 0), max(bc_bbox[1] - 50, 0),
-               min(bc_bbox[2] + 50, paper_size[0]), min(bc_bbox[3] + 50, paper_size[1]))
-  # XXX
-  form_img_fixed.save('/tmp/debug_fixed.tif', 'TIFF')
+  crop_buffer = 50
+  crop_bbox = (max(bc_bbox[0] - crop_buffer, 0),
+               max(bc_bbox[1] - crop_buffer, 0),
+               min(bc_bbox[2] + crop_buffer, paper_size[0]),
+               min(bc_bbox[3] + crop_buffer, paper_size[1]))
+  # We don't need to crop when using the local tool. It will find the barcode
+  # effectively.
   #form_id = bc.readbarcode(form_img_fixed.crop(crop_bbox))
-  # XXX
   form_id = bc.readbarcode(form_img_fixed)
-  #
+
   # Grab the form data
   formdata_url = '%s/surveys/%s/forms/%s' % (API_BASE, survey_id, form_id)
+  print 'Getting form data from %s' % formdata_url
   try:
-    print 'Getting form data from %s' % formdata_url
-    form_json = urllib2.urlopen(formdata_url).read()
-  except urllib2.HTTPError, e:
-    print "HTTP error: %d" %e.code
+    data = api_get(formdata_url)
+  except APIError, e:
     return 2
-  except urllib2.URLError, e:
-    print "Network error: %s" % e.reason.args[1]
-    return 2
+  form_data = data['form']
   #
   print 'Processing answers'
-  #
-  # Read form data
-  form_data = json.loads(form_json)['form']
-  #
+
   # Iterate over each parcel on the form
   responses = []
   for parcel_piece in form_data['parcels']:
@@ -237,7 +189,7 @@ def record_form(survey_id, img_id, noact=False, paperinfo=None):
     for bs in formset.sets:
       answers[bs.name] = bs.get_single_answer(form_img_fixed)
     responses.append({'parcel_id' : parcel_piece['parcel_id'], 'responses' : answers})
-  #
+
   # Print the data that we got
   print 'Survey ID: %s' % survey_id
   print 'Form ID: %s' % form_id
@@ -248,20 +200,17 @@ def record_form(survey_id, img_id, noact=False, paperinfo=None):
     return
   response_url = '%s/surveys/%s/responses' % (API_BASE, survey_id)
   try:
-    print 'Posting responses to %s' % survey_url
+    print 'Posting responses to %s' % response_url
     data = json.dumps({'responses' : responses})
     print 'JSON data:\n%s' % data
-    req = urllib2.Request(response_url, headers={'Content-Type':'application/json'}, data=data)
-    api_resp = urllib2.urlopen(req).read()
-  except urllib2.HTTPError, e:
-    print "HTTP error: %d" %e.code
-    return 2
-  except urllib2.URLError, e:
-    print "Network error: %s" % e.reason.args[1]
+    headers = {'Content-Type' : 'application/json'}
+    r = requests.post(response_url, headers=headers, data=data)
+    api_resp = r.text
+  except requests.exceptions.RequestException, e:
     return 2
   print 'Response from server:'
   print api_resp
-  #
+
   # Update status to 'complete'
   try:
     update_status(survey_id, img_id, 'complete')
@@ -292,14 +241,15 @@ def main(argv=None):
   if len(args) > 0:
     # process arguments
     img_id = args[0]
-    return record_form(survey_id, img_id, noact)
+    survey = get_survey(survey_id)
+    return record_form(survey, img_id, noact)
   elif survey_id is not None:
     # If we got a survey ID, get the pending images for that survey and process
     # each of them.
     survey = get_survey(survey_id)
     scans = get_scans(survey_id)
     for scan in scans:
-      ret = record_form(survey_id, scan['id'], noact, paperinfo=survey['paperinfo'])
+      ret = record_form(survey, scan['id'], noact, paperinfo=survey['paperinfo'])
       if (ret is not None) and (ret != 0):
         return ret
   else:
@@ -313,7 +263,7 @@ def main(argv=None):
       print
       scans = get_scans(survey_id)
       for scan in scans:
-        tmp_ret = record_form(survey_id, scan['id'], noact, paperinfo=survey['paperinfo'])
+        tmp_ret = record_form(survey, scan['id'], noact, paperinfo=survey['paperinfo'])
         if tmp_ret is not None and tmp_ret != 0:
           ret = tmp_ret
     return ret
@@ -344,7 +294,7 @@ def main(argv=None):
           print
           scans = get_scans(survey_id)
           for scan in scans:
-            ret = record_form(survey_id, scan['id'], noact, paperinfo=survey['paperinfo'])
+            ret = record_form(survey, scan['id'], noact, paperinfo=survey['paperinfo'])
             if (ret is not None) and (ret != 0):
               return ret
 
